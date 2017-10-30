478d9e49c2387d680ab70ba114a5a07560ecf810
superzdd <superzdd@sina.com>
2017-10-31 00:39:33 +0800

    add google map and newyork times

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
+var googleAPI = 'AIzaSyDwuOfUbyR5Q070toOp8wJEfaR18DQe7gY';
+var googleMapURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=';
+var streetCityStrDemo = 'Chagrin Falls, OH';
+var newYorkTimeAPI = '0f4e6ee4c2834a35b3dfa268f4ed5a61';
 
 function loadData() {
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var $greeting = $('#greeting');
+	var $body = $('body');
+	var $wikiElem = $('#wikipedia-links');
+	var $nytHeaderElem = $('#nytimes-header');
+	var $nytElem = $('#nytimes-articles');
+	var $greeting = $('#greeting');
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+	// clear out old data before new request
+	$wikiElem.text("");
+	$nytElem.text("");
 
-    // load streetview
+	// load streetview
 
-    // YOUR CODE GOES HERE!
+	// YOUR CODE GOES HERE!
 
-    return false;
+	var $streetStr = $("#street").val();
+	var $cityStr = $("#city").val();
+
+	console.log('get street :' + $streetStr + ', city : ' + $cityStr);
+
+	var imgUrl = googleMapURL + streetCityStrDemo + '&key=' + googleAPI;
+
+	$body.append('<img class="bgimg" src="' + imgUrl + '">');
+
+	return false;
 };
 
 $('#form-container').submit(loadData);
